269056e0d13e99e4f77a0cd0544a908d461c1e29
Luis Velasco <=>
2012-08-04 14:55:51 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Globasico::Application.config.secret_token = 'cd6137d803655feafad9345d83cb026747d82c19b4e4a1183b2c8d50d8dd344b6e23649ea3c38ed26ddd18452a08e2ea6f92826fdc2c1ffa87eb40c70a20c3fd'
